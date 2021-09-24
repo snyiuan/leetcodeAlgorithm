a642195a16a507f0f0b5fb3759ac25e19e2c1ef6
snyiuan <996463517@qq.com>
2021-09-24 10:42:37 +0800

    12

---
+++ L117connect.java
@@ -0,0 +1,14 @@
+package leetCode;
+
+/**
+ * @author zzq
+ * @create 2021-09-22-17:08
+ */
+public class L117connect {
+    public static void main(String[] args) {
+
+    }
+    public Node connect(Node root) {
+        return new Node();
+    }
+}
